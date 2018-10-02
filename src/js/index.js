d0347fc770d62f69cba392fd22eba09be81d1131
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-10-02 16:20:26 -0500

    v-for loop

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
 var app = new Vue({
   el:'#app',
   data: {
-    seen: true
+    seen: true,
+    todos: [
+      { text: 'Learn Javascript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome'}
+    ]
   }
-})
+});
