4436e4c6833431250bfa7715e377f34a11e9ba8f
fiorellaeffio <fiorella.effio@unmsm.edu.pe>
2018-10-03 04:10:42 -0500

    adding a todo list

---
+++ src/components/TodoList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done?"
+    v-model="newTodo">
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      {{ todo.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  data () {
+    return {
+      newTodo: '',
+      todos: [
+        {
+          'id': 1,
+          'title': 'Finish Vue Screenshot',
+          'completed': false,
+        },
+        {
+          'id': 2,
+          'title': 'Take over world',
+          'completed': false,
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style language="scss">
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+    &:focus {
+      outline: 0;
+    }
+  }
+</style>
